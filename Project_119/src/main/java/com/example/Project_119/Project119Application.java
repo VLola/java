eee457a7bd320524e5bbb85d2f5512dc204dfb6e
VLola <valiklolanew@gmail.com>
2023-03-17 13:07:57 +0200

    Revert "fix"
    This reverts commit 403196d4208d1729371f04740bc9514c0f0f4c91.

--- Project_119/src/main/java/com/example/Project_119/Project119Application.java
+++ Project_119/src/main/java/com/example/Project_119/Project119Application.java
@@ -3,14 +3,8 @@ package com.example.Project_119;
 import com.example.Project_119.controllers.WebController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class Project119Application {
 
 	public static void main(String[] args)
@@ -18,14 +12,5 @@ public class Project119Application {
 		new WebController();
 		SpringApplication.run(Project119Application.class, args);
 	}
-//
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/getMoonPhase").allowedOrigins("http://localhost:8080");
-//			}
-//		};
-//	}
+
 }
--- Project_119/src/main/java/com/example/Project_119/controllers/WebController.java
+++ Project_119/src/main/java/com/example/Project_119/controllers/WebController.java
@@ -1,57 +1,21 @@
 package com.example.Project_119.controllers;
+import com.example.Project_119.models.Trip;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 
 
 @RestController
 public class WebController {
-//    CalculatorController calculatorController = new CalculatorController();
-//    @GetMapping("/getFare")
-//    public Trip getFare(@RequestParam(value = "distance") double distance, @RequestParam(value = "consumption") double consumption, @RequestParam(value = "price") double price) {
-//        return calculatorController.getFare(distance, consumption, price);
-//    }
-//    @GetMapping("/getFuelConsumption")
-//    public double getFuelConsumption(@RequestParam(value = "fuel") double fuel, @RequestParam(value = "distance") double distance) {
-//        return calculatorController.getFuelConsumption(fuel, distance);
-//    }
-//    @GetMapping("/getHowMuchFuelIsEnough")
-//    public double getHowMuchFuelIsEnough(@RequestParam(value = "consumption") double consumption, @RequestParam(value = "fuel") double fuel) {
-//        return calculatorController.getHowMuchFuelIsEnough(consumption, fuel);
-//    }
-//    @CrossOrigin(origins = "http://localhost:8080")
-    @GetMapping("/getMoonPhase")
-    public String getMoonPhase() {
-        return Get();
+    CalculatorController calculatorController = new CalculatorController();
+    @GetMapping("/getFare")
+    public Trip getFare(@RequestParam(value = "distance") double distance, @RequestParam(value = "consumption") double consumption, @RequestParam(value = "price") double price) {
+        return calculatorController.getFare(distance, consumption, price);
     }
-    @PostMapping("/postMoonPhase")
-    public String postMoonPhase(@RequestParam(value = "date") String date) {
-
-        return Post(LocalDateTime.parse(date));
+    @GetMapping("/getFuelConsumption")
+    public double getFuelConsumption(@RequestParam(value = "fuel") double fuel, @RequestParam(value = "distance") double distance) {
+        return calculatorController.getFuelConsumption(fuel, distance);
     }
-    public String Post(LocalDateTime now){
-        if(now.getDayOfMonth()<=6) return "Waxing Crescent";
-        else if(now.getDayOfMonth()==7) return "Full moon";
-        else if(now.getDayOfMonth()>7 && now.getDayOfMonth()<=14) return "Waning moon";
-        else if(now.getDayOfMonth()>15) return "Last quarter";
-        else if(now.getDayOfMonth()>15 && now.getDayOfMonth()<=20) return "Waning month";
-        else if(now.getDayOfMonth()==21) return "New moon";
-        else if(now.getDayOfMonth()>21 && now.getDayOfMonth()<=27) return "Growing month";
-        else if(now.getDayOfMonth()>27 && now.getDayOfMonth()<30) return "First quarter";
-        else if(now.getDayOfMonth()>=30) return "First quarter";
-        else return "Error";
-    }
-    public String Get(){
-        LocalDateTime now = LocalDateTime.now();
-        if(now.getDayOfMonth()<=6) return "Waxing Crescent";
-        else if(now.getDayOfMonth()==7) return "Full moon";
-        else if(now.getDayOfMonth()>7 && now.getDayOfMonth()<=14) return "Waning moon";
-        else if(now.getDayOfMonth()>15) return "Last quarter";
-        else if(now.getDayOfMonth()>15 && now.getDayOfMonth()<=20) return "Waning month";
-        else if(now.getDayOfMonth()==21) return "New moon";
-        else if(now.getDayOfMonth()>21 && now.getDayOfMonth()<=27) return "Growing month";
-        else if(now.getDayOfMonth()>27 && now.getDayOfMonth()<30) return "First quarter";
-        else if(now.getDayOfMonth()>=30) return "First quarter";
-        else return "Error";
+    @GetMapping("/getHowMuchFuelIsEnough")
+    public double getHowMuchFuelIsEnough(@RequestParam(value = "consumption") double consumption, @RequestParam(value = "fuel") double fuel) {
+        return calculatorController.getHowMuchFuelIsEnough(consumption, fuel);
     }
 }
