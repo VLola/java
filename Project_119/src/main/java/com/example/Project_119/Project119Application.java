403196d4208d1729371f04740bc9514c0f0f4c91
VLola <valiklolanew@gmail.com>
2023-03-17 12:55:33 +0200

    fix

--- Project_119/src/main/java/com/example/Project_119/Project119Application.java
+++ Project_119/src/main/java/com/example/Project_119/Project119Application.java
@@ -3,8 +3,14 @@ package com.example.Project_119;
 import com.example.Project_119.controllers.WebController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Project119Application {
 
 	public static void main(String[] args)
@@ -12,5 +18,14 @@ public class Project119Application {
 		new WebController();
 		SpringApplication.run(Project119Application.class, args);
 	}
-
+//
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/getMoonPhase").allowedOrigins("http://localhost:8080");
+//			}
+//		};
+//	}
 }
